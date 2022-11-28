8fdb2a88848279023dac30438644e2197bf845d4
Juan I. Zumer <jizumer@gmail.com>
2022-11-28 18:55:02 +0100

    Basic project scaffolding

---
+++ src/main/java/com/github/jizumer/DeveloperResource.java
@@ -0,0 +1,16 @@
+package com.github.jizumer;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/developers")
+public class DeveloperResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getDeveloper() {
+        return "Basic developer info";
+    }
+}
