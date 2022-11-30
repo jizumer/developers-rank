47c1bec37c0e6da16e6f21ed4fd46e27991d644d
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-11-30 16:09:03 +0100

    Add developersStore to DeveloperService

--- src/main/java/com/github/jizumer/DeveloperService.java
+++ src/main/java/com/github/jizumer/DeveloperService.java
@@ -1,9 +1,15 @@
 package com.github.jizumer;
 
+import java.util.List;
 import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class DeveloperService {
+
+    private List<Developer> developersStore = List.of(
+            Developer.builder().id(1).name("John Doe").username("john.doe")
+                .email("john.doe@test.com").build()
+    );
 
     public DeveloperResponse findDeveloperById(int id) {
         return DeveloperResponse.builder().id(id).name("John Doe").username("john.doe")
