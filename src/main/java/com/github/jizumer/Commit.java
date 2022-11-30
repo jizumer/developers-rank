a8d856013384e99c9d1fbd7cc2f75810af14596e
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-11-30 17:20:03 +0100

    Add properties fields to Commit class

--- src/main/java/com/github/jizumer/Commit.java
+++ src/main/java/com/github/jizumer/Commit.java
@@ -1,4 +1,17 @@
 package com.github.jizumer;
 
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
 public class Commit {
+    private LocalDateTime timestamp;
+    private String hash;
+    private String username;
+    private String repository;
+    private String branch;
+    private String comment;
+    private int lines;
 }
