4bc3197d199a8cbdd4211a11147e169ea89eb416
Carlos Barroso <carlos.barroso.baltasar@gmail.com>
2022-11-30 18:29:33 +0100

    Add new endpoint for returning all commits made by a developer

--- src/main/java/com/github/jizumer/CommitResource.java
+++ src/main/java/com/github/jizumer/CommitResource.java
@@ -1,7 +1,10 @@
 package com.github.jizumer;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -23,5 +26,32 @@ public class CommitResource {
         commitService.save(commit);
 
         return Response.status(Status.CREATED).build();
+    }
+
+    @GET
+    @Path("/developers/{username}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Commit> getAllCommitsByDeveloper(String username) {
+        final var firstCommit = Commit.builder()
+            .timestamp(LocalDateTime.parse("2022-11-29T10:24:22.876813"))
+            .hash("5f1bce8")
+            .username("john.doe")
+            .repository("developers-rank")
+            .branch("main")
+            .comment("This is my first commit")
+            .lines(2)
+            .build();
+
+        final var secondCommit = Commit.builder()
+            .timestamp(LocalDateTime.parse("2022-11-29T16:44:22.876813"))
+            .hash("1wetr42")
+            .username("john.doe")
+            .repository("developers-rank")
+            .branch("main")
+            .comment("This is my second commit")
+            .lines(2)
+            .build();
+
+        return List.of(firstCommit, secondCommit);
     }
 }
--- src/test/java/com/github/jizumer/CommitResourceTest.java
+++ src/test/java/com/github/jizumer/CommitResourceTest.java
@@ -2,6 +2,7 @@ package com.github.jizumer;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.Mockito.verify;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -68,5 +69,48 @@ class CommitResourceTest {
             .statusCode(201);
 
         assertThat(commitService.findAllCommits()).contains(expectedSavedCommit);
+    }
+
+    @Test
+    void shouldAllCommitsMadeByADeveloper() {
+        final var firstCommit = Commit.builder()
+            .timestamp(LocalDateTime.parse("2022-11-29T10:24:22.876813"))
+            .hash("5f1bce8")
+            .username("john.doe")
+            .repository("developers-rank")
+            .branch("main")
+            .comment("This is my first commit")
+            .lines(2)
+            .build();
+
+        final var secondCommit = Commit.builder()
+            .timestamp(LocalDateTime.parse("2022-11-29T16:44:22.876813"))
+            .hash("1wetr42")
+            .username("john.doe")
+            .repository("developers-rank")
+            .branch("main")
+            .comment("This is my second commit")
+            .lines(10)
+            .build();
+
+        final var commitFromAnotherDeveloper = Commit.builder()
+            .timestamp(LocalDateTime.parse("2022-11-23T16:44:22.876813"))
+            .hash("qws2341")
+            .username("charles.barrow")
+            .repository("developers-rank")
+            .branch("main")
+            .comment("This is one commit from Charles")
+            .lines(21)
+            .build();
+
+        commitService.save(firstCommit);
+        commitService.save(secondCommit);
+        commitService.save(commitFromAnotherDeveloper);
+
+        given()
+            .when().get("/commits/developers/john.doe")
+            .then()
+            .statusCode(200)
+            .body("$.size()", is(2));
     }
 }
